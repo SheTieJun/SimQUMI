c20c1014f574d57c2b4f540afee4dd392475cd91
shetj <tiejun.she@lizhiweike.com>
2019-09-05 15:49:19 +0800

    Initial commit

---
+++ app/src/main/java/me/shetj/qumidemo/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.shetj.qumidemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.shetj.qumi.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
